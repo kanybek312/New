ab84a9756078d7e50740ad1308605b5de1e5f475
Kanybek Mukalaev <you@example.com>
2020-07-23 08:27:53 +0200

    Adding another code

--- Sequence.java
+++ Sequence.java
@@ -3,6 +3,17 @@ import java.util.Scanner;
 public class Sequence {
     public static void main (String args[]) {
         Scanner sc = new Scanner(System.in);
-         
+         int number = sc.nextInt();
+         int i = 0;
+         for (int k = 1; k <= number; k++) {
+             for ( int j = 0; j < k; j++) {
+                 if (i == number) {
+                     break;
+                 }
+                 System.out.print(k +" ");
+                 i++;
+             }
+
+         }
     }
 }
